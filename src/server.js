7af7e4e70600d9d8099d218a5e33576f452dea25
francisco <fbertolotto@fi.uba.ar>
2022-06-28 17:45:15 -0300

    adds api-key check for every endpoint

--- src/server.js
+++ src/server.js
@@ -1,9 +1,34 @@
 const config = require("./config");
 const services = require("./services/services")({ config });
 const routes = require("./routes");
+const axios = require("axios");
 
-// Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
+
+async function isTokenValid(token) {
+  const urlValidator = process.env.VALIDATION_URL + token;
+  try {
+    const response = await axios.get(urlValidator);
+    return response.status === 200;
+  } catch (err) {
+    return false;
+  }
+}
+
+fastify.addHook("onRequest", async (request, reply) => {
+  bearer_token = request.headers.authorization;
+  if (!bearer_token) {
+    reply.code(401).send({ error: "Unauthorized" });
+    return;
+  }
+
+  without_bearer = bearer_token.split(" ");
+  token = without_bearer[1];
+  if (!(await isTokenValid(token))) {
+    reply.code(401).send({ error: "Unauthorized" });
+    return;
+  }
+});
 
 // Declares routes
 routes.forEach(route => fastify.route(route({ config, services })));
