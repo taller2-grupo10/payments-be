dffd71625cc778ef053fdc1e8c5fb0321a5a9a58
Mauro Santoni <mausantoni@gmail.com>
2022-07-14 10:52:02 -0300

    enable codecov cov run

--- entrypoint_test.sh
+++ entrypoint_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm run coverage
-#curl -Os https://uploader.codecov.io/v0.1.0_4653/linux/codecov
-#chmod +x codecov
-#./codecov -t 6c17d19d-7995-4107-a084-91fadfe7bd72
+curl -Os https://uploader.codecov.io/v0.1.0_4653/linux/codecov
+chmod +x codecov
+./codecov -t 63eade54-ea49-4fa5-8008-567bbc902a07
 exec "$@"
